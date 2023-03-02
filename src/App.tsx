8f0c4696838d2aab6d5dc6e7b24f4dc77121c952
ketyung <ketyung@techchee.com>
2023-03-02 14:38:19 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { CollectionView } from './views/CollectionView';
-//import { ItemsView } from 'pix0-react2-arch-test';
-//import { CollectionView } from 'pix0-react2-arch-test';
+//import { CollectionView } from './views/CollectionView';
+import { ItemsView } from 'pix0-react2-arch-test';
+import { CollectionView } from 'pix0-react2-arch-test';
+import { MintButton} from 'pix0-react2-arch-test';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App" style={{textAlign:"left"}}>
       <CollectionView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x"/>
+      <p>Collection View 2::<br/>
+      <CollectionView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x"/>
+      </p>
+
+      <MintButton/>
+      <p>Items::</p>
+      { <ItemsView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x" collection_name='Test NFT Collection 2'
+      collection_symbol='TNFT2'/>  }
     </div>
   );
 }
