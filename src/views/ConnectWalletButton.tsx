90ca4a04b659ee04d80dc249b6a5eea4585abe17
ketyung <ketyung@techchee.com>
2023-03-03 12:18:47 +0800

    update

--- src/views/ConnectWalletButton.tsx
+++ src/views/ConnectWalletButton.tsx
@@ -1,17 +1,16 @@
-import { FC } from "react";
+import { FC} from "react";
 import {connectWallet} from 'pix0-react2-arch-test';
 
 export const ConnectWalletButton : FC = () =>{
 
-    return <button style={{background:"#348",color:"white",width:"150px",
+
+    return <div><button style={{background:"#348",color:"white",width:"150px",
     padding:"10px",margin:"10px", borderRadius:"20px",border:"0px", cursor:"pointer"}}
     onClick={async (e)=>{
 
         e.preventDefault();
         try {
-            let c = await connectWallet();
-            console.log("Connected.wallet.params::",c);
-    
+            await connectWallet();
         }
         catch(e : any){
             alert("Error.connect.wallet::"+ e);
@@ -19,5 +18,21 @@ export const ConnectWalletButton : FC = () =>{
         }
     }}>    
     Connect Wallet
+    </button><br/>
+    <button style={{background:"#375",color:"white",width:"150px",
+    padding:"10px",margin:"10px", borderRadius:"20px",border:"0px", cursor:"pointer"}}
+    onClick={async (e)=>{
+
+        e.preventDefault();
+        try {
+            await connectWallet(true);
+        }
+        catch(e : any){
+            alert("Error.connect.wallet::"+ e);
+            console.log("Error.connect.wallet::", e);
+        }
+    }}>    
+    Connect Wallet Experimental Chain
     </button>
+    </div>
 }
