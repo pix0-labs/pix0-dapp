3f2ac6680d0147fe7e170f02b943fb4554f0be10
techchee <ketyung@techchee.com>
2023-03-05 12:18:46 +0800

    update

--- src/views/View.tsx
+++ src/views/View.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { useWalletConnection } from "pix0-react2-arch-test";
+import {useWalletConnection} from "pix0-react2-arch-test";
 import { ItemsView, CollectionView } from 'pix0-react2-arch-test';
 import { MintButton, ConnectButton, BalanceView} from 'pix0-react2-arch-test';
 import copyIcon from '../images/copy.png';
@@ -10,21 +10,22 @@ export const View : FC = () =>{
     const {connected} = useWalletConnection();
 
     return <>
-    { connected ? <>
-        <ConnectButton experimental={true}/>
-      <p>&nbsp;</p>
-      <BalanceView displayDecimals={3} copyIcon={<img src={copyIcon} 
-      style={{marginLeft:"4px",width:"20px",height:"20px"}}/>}/>
-     
-    </> : <>  <CollectionView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x"/>
+    { connected ?  <>  <CollectionView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x"/>
       <p>Collection View 2::<br/>
       <CollectionView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x"/>
       </p>
+
 
       <MintButton/>
       <p>Items::</p>
       { <ItemsView owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x" collection_name='Test NFT Collection 2'
       collection_symbol='TNFT2'/>  }
-    </>}
+    </> : <>
+        <ConnectButton experimental={true}/>
+      <p>&nbsp;</p>
+      <BalanceView displayDecimals={3} copyIcon={<img src={copyIcon} 
+      style={{marginLeft:"4px",width:"20px",height:"20px"}}/>}/>
+     <p>Connected....:{connected}</p>
+    </> }
     </>
 }
