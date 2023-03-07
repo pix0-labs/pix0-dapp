39770d8f5e1d1c1191768ebb175dbae1ee13c9d3
techchee <ketyung@techchee.com>
2023-03-07 11:16:24 +0800

    update

--- src/views/modules/MintNFTView.tsx
+++ src/views/modules/MintNFTView.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { motion } from 'framer-motion';
 import { COMMON_PANEL_CLASS_NAME } from "./config";
+import { MintButton } from "pix0-react2-arch-test";
 
 export const MintNFTView : FC = () =>{
 
@@ -8,5 +9,8 @@ export const MintNFTView : FC = () =>{
     className={COMMON_PANEL_CLASS_NAME}
     initial={{ opacity: 0, scale: 0.5 }}
     animate={{ opacity: 1, scale: 1 }}
-    transition={{ duration: 0.5 }}>Mint An NFT</motion.div>
+    transition={{ duration: 0.5 }}>Mint An NFT<br/>
+    <p> <MintButton collection_owner="archway12pcytur9del2t5wm93t8kuqakvf9yk9wzt4w0x" collection_name='Test NFT Collection 2'
+      collection_symbol='TNFT2'/></p>
+    </motion.div>
 }
