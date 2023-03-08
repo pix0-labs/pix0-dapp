198d7a8df64ffc388ed74b0c240aaaa28f7daa65
techchee <ketyung@techchee.com>
2023-03-08 11:07:53 +0800

    update

--- src/views/modules/NFTsView.tsx
+++ src/views/modules/NFTsView.tsx
@@ -1,17 +1,19 @@
 import { FC, useState, useCallback, useEffect } from "react";
 import useCollectionContract from "pix0-react2-arch-test";
+import { Loader} from 'pix0-react2-arch-test';
 import { CommonMessageDiv } from "../CommonMessageDiv";
 
 export const NFTsView : FC = () =>{
 
-    const [tokens, setTokens] = useState<string[]>([]);
+    const [tokens, setTokens] = useState<string[]>();
 
     const {getMintedTokensByOwner} = useCollectionContract();
 
+   
     const fetchTokens = useCallback (async () =>{
-        let nfts = await getMintedTokensByOwner({});
+        
+       let nfts = await getMintedTokensByOwner({});
 
-        console.log("Nfts...x", nfts);
         setTokens(nfts);
     }, []);
 
@@ -22,9 +24,15 @@ export const NFTsView : FC = () =>{
 
     return <div>
     {
-        tokens.length > 0 ?
 
-        tokens.map((t, i)=>{
+        tokens === undefined ?
+
+        <div className="mx-auto w-2/5 mt-20 bg-gray-500 rounded-3xl p-4 text-center"><Loader/></div>
+
+        :
+
+        tokens.length > 0 ?
+        tokens?.map((t, i)=>{
 
             return <div>{i}. {t}</div>
 
