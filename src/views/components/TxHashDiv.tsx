d23dcd02229a2842c77ffa5129b5b8d784475290
ketyung <ketyung@techchee.com>
2023-03-10 10:46:16 +0800

    update

--- src/views/components/TxHashDiv.tsx
+++ src/views/components/TxHashDiv.tsx
@@ -19,13 +19,13 @@ export const TxHashDiv : FC <props> = ({
 
         if (txHash instanceof Error){
 
-            return <CommonMessageDiv className="text-left w-4/5 p-2 bg-red-600 text-gray-100 rounded-2xl mb-2"
+            return <CommonMessageDiv className="text-left w-4/5 p-2 bg-red-600 text-gray-100 rounded-3xl mb-2"
             transitionDuration={0.32}>{txHash.message}</CommonMessageDiv>
         }
         else {
     
-            return <CommonMessageDiv className="text-left w-4/5 p-2 bg-cyan-800 text-gray-100 rounded-2xl mb-2"
-            transitionDuration={0.32}><a target="_blank" 
+            return <CommonMessageDiv className="text-left w-4/5 p-2 bg-cyan-800 text-gray-100 rounded-3xl mb-2"
+            transitionDuration={0.32}>Success! View TX : <a target="_blank" 
             href={`${TX_URL_PREFIX}${txHash}`}>{shortenStringTo(txHash,10)}</a>
             </CommonMessageDiv>
         }
