edb54aa80ab6f77a8b422465b9378786fcb96c7a
techchee <ketyung@techchee.com>
2023-03-29 19:59:21 +0800

    update

--- src/views/modules/collector/CollectiblesView.tsx
+++ src/views/modules/collector/CollectiblesView.tsx
@@ -1,17 +1,30 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { motion } from 'framer-motion';
 import { NFTsView } from "./NFTsView";
 import { TopMenu } from "./TopMenu";
 import { COMMON_PANEL_CLASS_NAME } from "../config";
 
+export enum ViewType {
+
+    COLLECTIBLES, 
+
+    MINT_FROM_NBH,
+
+    SIMPLE_MINT,
+}
+
+
 export const CollectiblesView : FC = () =>{
+
+    const [viewType, setViewType] = useState<ViewType>(ViewType.COLLECTIBLES);
+
 
     return <motion.div 
     className={COMMON_PANEL_CLASS_NAME}
     initial={{ opacity: 0, scale: 0.5 }}
     animate={{ opacity: 1, scale: 1 }}
     transition={{ duration: 0.5 }}>
-        <TopMenu/>
+        <TopMenu setViewType={setViewType} viewType={viewType}/>
         <NFTsView/>
     </motion.div>
 }
--- src/views/modules/collector/TopMenu.tsx
+++ src/views/modules/collector/TopMenu.tsx
@@ -1,12 +1,46 @@
 import { FC } from "react";
+import { ViewType } from "./CollectiblesView";
+import { motion } from "framer-motion";
 
 
-export const TopMenu : FC = () =>{
+type props = {
+
+    viewType? : ViewType,
+
+    setViewType? : (viewType : ViewType) => void, 
+}
+
+export const TopMenu : FC <props> = ({
+    viewType, setViewType
+}) =>{
+
+    const bgc = (vtype : ViewType) => {
+
+        return vtype === viewType ? "bg-gray-800" : "bg-gray-500";
+    }   
+
 
     return <div className="bg-gray-700 p-2 text-left w-3/5 shadow-md rounded-3xl ml-2">
-    <div className="inline-block p-2 rounded-3xl bg-gray-500 mx-2">Your Collectibles</div>
-    <div className="inline-block p-2 rounded-3xl bg-gray-800 mx-2">Mint NFTs from Neighborhood</div>
-    <div className="inline-block p-2 rounded-3xl bg-gray-800 mx-2">Simple Mint</div>
+    <motion.div className={`cursor-pointer inline-block p-2 rounded-3xl ${bgc(ViewType.COLLECTIBLES)} mx-2`}
+     onClick={()=>{
+        if (setViewType)
+            setViewType(ViewType.COLLECTIBLES);
+     }}   
+     transition={{ duration: 0.8, type: "tween" }}>Your Collectibles</motion.div>
+    <motion.div className={`cursor-pointer inline-block p-2 rounded-3xl ${bgc(ViewType.MINT_FROM_NBH)} mx-2`}
+     onClick={()=>{
+        if (setViewType)
+            setViewType(ViewType.MINT_FROM_NBH);
+     }}   
+     transition={{ duration: 0.8, type: "tween" }}>Mint From Neighborhood</motion.div>
+    <motion.div className={`cursor-pointer inline-block p-2 rounded-3xl ${bgc(ViewType.SIMPLE_MINT)} mx-2`}
+     onClick={()=>{
+        if (setViewType)
+            setViewType(ViewType.SIMPLE_MINT);
+     }}   
+     transition={{ duration: 0.8, type: "tween" }}>Mint From Neighborhood</motion.div>
+ 
+ 
    </div>
 
 }
