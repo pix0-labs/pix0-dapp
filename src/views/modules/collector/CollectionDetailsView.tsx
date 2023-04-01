fb60c98e953b96a681b920a387181c9383d638e3
techchee <ketyung@techchee.com>
2023-04-01 09:24:22 +0800

    update

--- src/views/modules/collector/CollectionDetailsView.tsx
+++ src/views/modules/collector/CollectionDetailsView.tsx
@@ -16,26 +16,26 @@ export const CollectionDetailsView : FC <props> = ({
     const {img} = useCollectionRandomImg(collection);
 
     const imgView = img ? <a href={img}
-    target="_blank"><img className="mr-3" src={img} style={{maxHeight:"260px",maxWidth:"260px",display:"block"}}  
+    target="_blank"><img className="mx-auto" src={img} style={{maxHeight:"260px",maxWidth:"260px",display:"block"}}  
     placeholder={placeholder}/></a> :
-    <img src={placeholder} className="mr-3" style={{maxHeight:"260px",maxWidth:"260px",display:"block"}} 
+    <img src={placeholder} className="mx-auto" style={{maxHeight:"260px",maxWidth:"260px",display:"block"}} 
     placeholder={placeholder}/>;
 
 
-    return <div className="pl-20 text-left pt-2">
-        <div className="p-2 rounded-2xl bg-gray-700 text-gray-200 mb-2">
+    return <div className="w-4/5 text-left pt-2 bg-gray-800 text-center rounded-3xl p-4 mt-4">
+        <div className="p-2 rounded-2xl bg-gray-700 text-gray-200 mb-10">
             {collection.name}
         </div>
-        <div>
+        <div className="mb-10">
             {imgView}
         </div>
 
         {collection.description && 
-        <div className="p-2 rounded-2xl bg-gray-700 text-gray-200 mb-2">
+        <div className="p-2 rounded-2xl bg-gray-700 text-gray-200 mb-10">
             {collection.description}
         </div>}
        
-        <div className="p-2 mb-2">
+        <div className="p-2 mb-10">
             <button className="rounded-3xl bg-green-900 font-bold p-2 text-gray-200 w-64">
                 Mint NFT
             </button>
