48a4ccd179a8692c76b59155cad7e6c7eda26511
techchee <ketyung@techchee.com>
2023-04-01 17:54:25 +0800

    update

---
+++ src/views/modules/collector/NFTListView.tsx
@@ -0,0 +1,47 @@
+import { FC, useState, useCallback, useEffect } from "react";
+import useCollectionContract from "pix0-react";
+import { PulseLoader as Loader} from 'react-spinners';
+import { NFTView } from "./NFTView";
+import { CommonAnimatedDiv } from "../../components/CommonAnimatedDiv";
+import { CommonMessageDiv } from "../../components/CommonMessageDiv";
+
+export const NFTListView : FC = () =>{
+
+    const [tokens, setTokens] = useState<string[]>();
+
+    const {getMintedTokensByOwner} = useCollectionContract();
+
+   
+    const fetchTokens = useCallback (async () =>{
+        
+       let nfts = await getMintedTokensByOwner({});
+
+        setTokens(nfts);
+    }, []);
+
+
+    useEffect(()=>{
+        fetchTokens();
+    },[fetchTokens]);
+
+    return <CommonAnimatedDiv className="w-full p-2 text-center mx-auto">
+     {
+
+        tokens === undefined ?
+
+        <div className="text-left p-2"><Loader color="#eee"/></div>
+        :
+        tokens.length > 0 ?
+
+        <div className="flex flex-wrap text-center">{
+            tokens?.map((t, _i)=>{
+                return <NFTView key={`Nft_${_i}`} tokenId={t} index={_i}/>
+            })
+        }</div>
+        
+        : <CommonMessageDiv>You do NOT have any collectibles yet. Go Mint or Buy some NFTs</CommonMessageDiv>
+    }
+
+    </CommonAnimatedDiv>
+
+}
--- src/views/modules/collector/NFTsView.tsx
+++ src/views/modules/collector/NFTsView.tsx
@@ -1,46 +1,41 @@
-import { FC, useState, useCallback, useEffect } from "react";
-import useCollectionContract from "pix0-react";
+import { FC, useState } from "react";
 import { PulseLoader as Loader} from 'react-spinners';
 import { NFTView } from "./NFTView";
 import { CommonMessageDiv } from "../../components/CommonMessageDiv";
+import { NFTListView } from "./NFTListView";
+import { NFTDetailsView } from "./NFTDetailsView";
+
+
+export enum ViewType {
+
+    LIST, 
+
+    DETAIL,
+
+}
+
 
 export const NFTsView : FC = () =>{
 
-    const [tokens, setTokens] = useState<string[]>();
+    const [viewType, setViewType] = useState<ViewType>(ViewType.LIST);
 
-    const {getMintedTokensByOwner} = useCollectionContract();
+    const [tokenId, setTokenId] = useState<string>("");
 
-   
-    const fetchTokens = useCallback (async () =>{
-        
-       let nfts = await getMintedTokensByOwner({});
+    const switchView = () =>{
 
-        setTokens(nfts);
-    }, []);
+        switch (+viewType) {
 
+            case ViewType.LIST :
 
-    useEffect(()=>{
-        fetchTokens();
-    },[fetchTokens]);
+                return <NFTListView/>;
 
-    return <div className="w-full p-2 text-center mx-auto">
-     {
+            case ViewType.DETAIL :
 
-        tokens === undefined ?
+                return <NFTDetailsView tokenId={tokenId}/>
+        }
 
-        <div className="text-left p-2"><Loader color="#eee"/></div>
-        :
-        tokens.length > 0 ?
-
-        <div className="flex flex-wrap text-center">{
-            tokens?.map((t, _i)=>{
-                return <NFTView key={`Nft_${_i}`} tokenId={t} index={_i}/>
-            })
-        }</div>
-        
-        : <CommonMessageDiv>You do NOT have any collectibles yet. Go Mint or Buy some NFTs</CommonMessageDiv>
     }
 
-    </div>
+    return <>{switchView()}</>
 
 }
