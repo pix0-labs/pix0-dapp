ad891b0ccb793b07eb8ce0529014b2da5ea8bbed
techchee <ketyung@techchee.com>
2023-04-09 19:36:56 +0800

    update

--- src/views/components/ToggleButton.tsx
+++ src/views/components/ToggleButton.tsx
@@ -16,13 +16,14 @@ export const ToggleButton : FC <props> = ({
     size, x, y, onClick
 }) =>{
 
-    return  <button className={`fixed ${x?.direction ?? "top"}-${x?.margin ?? 4} 
-    ${y?.direction ?? "left"}-${y?.margin ?? 40} ml-2 p-2 bg-gray-500 text-white rounded-3xl`}
+    return  <button className={`fixed ${x?.direction ?? "top"}-${x?.margin ?? 10} 
+    ${y?.direction ?? "left"}-${y?.margin ?? 20} ml-2 p-2 bg-gray-500 
+    text-white rounded-3xl h-${size?.w ?? 20} w-${size?.w ?? 20}`}
     onClick={(e)=>{
         e.preventDefault();
         if ( onClick)onClick();
     }}>
-        <svg className={`h-${size?.w ?? 2} w-${size?.w ?? 2}`} viewBox="0 0 20 20" fill="currentColor">
+        <svg viewBox="0 0 40 40" fill="white">
         <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h8a1 1 0 
         110 2H4a1 1 0 01-1-1zM3 16a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
         </svg>
