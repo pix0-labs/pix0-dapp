1a5aba149fc03d1f486f7d04e8c0afddd8ddb4e6
techchee <ketyung@techchee.com>
2023-04-12 16:57:39 +0800

    update

--- src/views/modules/market/MarketView.tsx
+++ src/views/modules/market/MarketView.tsx
@@ -7,5 +7,6 @@ export const MarketView : FC = () =>{
     return <CommonAnimatedDiv className="ml-0 h-full">
     <SellOffersByCollections/>
     <SellOffersListView/>
+    <div className="h-32">&nbsp;</div>
     </CommonAnimatedDiv>
 }
