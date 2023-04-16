109b8fb56cca5bf66a7914a7bb9ca7d653af1b4c
techchee <ketyung@techchee.com>
2023-04-16 08:00:32 +0800

    update

---
+++ src/views/modules/collector/CreateSellOfferForm.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import { Nft} from 'pix0-js';
+import { TextField, commonTextfieldClassName } from "../../components/TextField";
+import placeholder from '../../../images/placeholder2.png';
+
+
+export type props = {
+
+    token? : Nft,
+}
+
+
+export const CreateSellOfferForm : FC <props> = ({
+    token
+}) =>{
+
+
+    const imgView = token?.extension.image ? <a href={token.extension.image}
+    target="_blank"><img className="rounded mx-auto" src={token.extension.image} 
+    style={{height:"120px",width:"120px",display:"block",border:"5px solid rgba(240,240,250,.35)"}}  
+    placeholder={placeholder}/></a> :
+    <img src={placeholder} className="mx-auto rounded-full" style={{height:"200px",width:"200px",display:"block"}} 
+    placeholder={placeholder}/>;
+
+
+    return <div className="modal rounded bg-gray-800 w-full text-center text-gray-100 p-2">
+    <div className="mb-2 header">Create Sell Offer For</div>
+    <div className="content">
+        <div className="mb-1">{imgView}</div>
+        <div className="mb-6 font-bold text-sm">{token?.extension.name}</div>
+        <div className="mb-6"><TextField label={<span className="mr-2 font-bold text-base">Price:</span>} 
+        className={commonTextfieldClassName('w-2/12 text-base font-bold min-w-32')}
+        type="number" labelInline={true}/><span className="ml-1 font-bold">CONST</span></div>    
+
+         <div className="mb-4"><button className="bg-green-900 p-2 text-base font-bold rounded-3xl text-gray-100"
+         style={{minWidth:"120px"}}>Create</button></div>
+        
+    </div>
+    </div>;
+}
