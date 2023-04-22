8a3c020ff60f658adc4373d748445259f11ae7da
techchee <ketyung@techchee.com>
2023-04-22 10:01:29 +0800

    update

--- src/views/ConnectWalletView.tsx
+++ src/views/ConnectWalletView.tsx
@@ -4,6 +4,7 @@ import { ConnectButton } from "pix0-react";
 import useWalletState from "../hooks/useWalletState";
 import { Popup} from 'reactjs-popup';
 import { AiFillGithub } from "react-icons/ai";
+import { BsFiletypeDoc } from "react-icons/bs";
 import logo from '../images/pix0_logo1.png';
 
 
@@ -15,10 +16,10 @@ export const ConnectWalletView : FC = () =>{
 
 
     const connectAsMenu =  <Popup contentStyle={{background:"#222",minWidth:"240px"}} 
-    arrowStyle={{color:"#222"}}
+    arrowStyle={{display:"none"}}
     className="bg-gray-900 text-gray-300 w-64 p-4 m-4"
     trigger={<button className="bg-gray-500 hover:bg-cyan-900 rounded pl-2 pr-2 
-    text-xs font-bold text-gray-100">Connect As/To</button>} position="left center">
+    text-xs font-bold text-gray-100">Connect As/To</button>} position="top center">
         <div className="rounded hover:bg-gray-600 hover:cursor-pointer 
         bg-gray-800 text-gray-200 p-2"
         onClick={(e)=>{
@@ -46,6 +47,9 @@ export const ConnectWalletView : FC = () =>{
     className="mx-auto bg-red-600 text-gray-100 rounded-3xl 
     p-2 w-11/12 m-2 font-bold">{error?.message}</CommonAnimatedDiv>
     <img src={logo} className="h-auto mx-auto mb-4 mt-10" style={{minWidth:"220px"}}/>
+    <CommonAnimatedDiv className="mt-4 text-gray-100 text-sm font-bold w-3/5 mx-auto p-2"
+    duration={2.5}>A mini decentralized social network built on the Archway blockchain that 
+    connects NFT creators or artists with NFT collectors</CommonAnimatedDiv>
     <ConnectButton experimental={true}
     className="p-2 mt-10 mb-10 bg-gray-800 w-3/5 hover:bg-cyan-900 
     rounded-3xl mx-auto text-gray-100 font-bold"
@@ -60,10 +64,12 @@ export const ConnectWalletView : FC = () =>{
         setWalletConnected();
     }}/>
     <div className="mb-2 text-sm">{connectAsMenu}</div>
+    
     <CommonAnimatedDiv className="mt-4 text-gray-100 text-sm font-bold"
-    duration={3}>Now Live On Archway Testnet</CommonAnimatedDiv>
+    duration={4.5}>Now Live On Archway Testnet</CommonAnimatedDiv>
     <div className="mt-4 text-gray-100 text-xs"><a href="https://docs.pix0.xyz" 
-    target="_blank" className="mr-2">Documentations</a><a className="ml-2" href="https://github.com/pix0-labs" 
+    target="_blank" className="mr-4"><BsFiletypeDoc className="inline w-5 h-5"/></a>
+    <a className="ml-4" href="https://github.com/pix0-labs" 
     target="_blank"><AiFillGithub className="inline w-5 h-5"/></a></div>
     </CommonAnimatedDiv>
 }
