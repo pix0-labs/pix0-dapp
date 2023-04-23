59dab62c9735e4134ab271960f14bab9270db170
techchee <ketyung@techchee.com>
2023-04-23 15:36:25 +0800

    update

--- src/views/modules/market/SelNftForSo.tsx
+++ src/views/modules/market/SelNftForSo.tsx
@@ -12,21 +12,38 @@ export const SelNftForSo : FC = () =>{
 
     const [contractAddr, setContractAddr] = useState<string>();
 
-    
-    const {getMintedTokensByOwner} = useCollectionContract();
+    const [showContractAddr, setShowContractAddr] = useState(false);
 
+    const [loading, setLoading] = useState(false);
+    
+    const {getMintedTokensByOwner, getTokens} = useCollectionContract();
 
     const showOrHideContractAddr = () =>{
-        const e = document.getElementById("nft_contract_addr_id");
-
-        e?.classList.toggle("hidden");
+        setShowContractAddr(!showContractAddr);
     }
    
-    const fetchTokens = useCallback (async () =>{
-        
-       let nfts = await getMintedTokensByOwner({});
+    const fetchDefaultTokens = async () =>{
+        let nfts = await getMintedTokensByOwner({});
         setTokens(nfts);
-    }, []);
+    }
+
+    const fetchTokens = useCallback (async () =>{
+        fetchDefaultTokens();
+    }, [fetchDefaultTokens]);
+
+    const retrieveTokens = async () =>{
+
+        if (contractAddr && contractAddr.trim() !== "") {
+            setLoading(true);
+            let toks = await getTokens({}, contractAddr);
+            setTokens(toks);
+
+            setLoading(false);
+        }
+        else {
+            window.alert('Invalid contract address!');
+        }
+    }
 
 
     useEffect(()=>{
@@ -45,18 +62,32 @@ export const SelNftForSo : FC = () =>{
         tokens.length > 0 ?
         <>
         <div className="ml-1 rounded text-gray-100 p-2 font-bold bg-gray-900">Your Collectibales</div>    
-        <div className="text-sm text-gray-100 ml-1 text-left">Please be aware that we can only display the NFTs you own through our COLLECTION CONTRACT. 
+        {!showContractAddr && <div className="text-sm text-gray-100 ml-1 text-left">Please be aware that we can only display the NFTs you own through our COLLECTION CONTRACT. 
         If you own NFTs from a different contract, please click <a href="#" onClick={()=>{
             showOrHideContractAddr();
-        }}>here</a> to specify the contract address and retrieve them.</div>
-        <div className="hidden p-2 text-left" id="nft_contract_addr_id">
+        }}>here</a> to specify the contract address and retrieve them.</div>}
+        {showContractAddr && <div className="p-2 text-left">
         <TextField label={<div className="text-xs font-bold">NFT Contract Address:</div>} id="contract_addr" type="text" 
         placeholder="NFT Contract Address" className={commonTextfieldClassName("w-9/12 inline-block")}
         onChange={(e)=>{
             setContractAddr(e.target.value);
         }} value={contractAddr}/> 
-        <button className="ml-2 rounded-3xl pl-2 pr-2 font-bold text-sm text-gray-100 inline bg-gray-500">Fetch</button>
-        </div>
+        <button className="ml-2 rounded-3xl pl-2 pr-2 font-bold 
+        text-sm text-gray-100 inline bg-gray-500"
+        onClick={async (e)=>{
+            e.preventDefault();
+            await retrieveTokens();
+        }}
+        >{loading ? <Loader color="white" size={6}/> : <>Retrieve</>}</button>
+        
+        <button className="ml-2 rounded-3xl pl-2 pr-2 font-bold 
+        text-sm text-gray-100 inline bg-gray-500" onClick={async (e)=>{
+            e.preventDefault();
+            showOrHideContractAddr();   
+            await fetchDefaultTokens();
+        }}>Close</button>
+        
+        </div>}
         <div className="flex flex-wrap items-stretch">
         {
             tokens?.map((t, _i)=>{
